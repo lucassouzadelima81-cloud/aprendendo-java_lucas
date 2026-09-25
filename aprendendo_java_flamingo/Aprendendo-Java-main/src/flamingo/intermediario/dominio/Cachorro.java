7e83327a818252692b14f3f3ae0e1f9a4d525375
Lucas_its4 <lucassouzadelima81@gmail.com>
2026-09-24 21:11:19 -0300

    Add files via upload

---
+++ aprendendo_java_flamingo/Aprendendo-Java-main/src/flamingo/intermediario/dominio/Cachorro.java
@@ -0,0 +1,14 @@
+package Aprendendo.Java.main.src.flamingo.intermediario.dominio;
+
+public class Cachorro {
+    public String nome = "mequetrefe";
+    public String raca;
+    public int idade;
+    public double kilo;
+
+    //aprendo metodos
+
+    public void latir() {
+        System.out.println("Au Au");
+    }
+}
---
+++ aprendendo_java_flamingo/Aprendendo-Java-main/src/flamingo/intermediario/dominio/Carro.java
@@ -0,0 +1,7 @@
+package Aprendendo.Java.main.src.flamingo.intermediario.dominio;
+
+public class Carro {
+    public String nome;
+    public String marca;
+    public int ano;
+}
