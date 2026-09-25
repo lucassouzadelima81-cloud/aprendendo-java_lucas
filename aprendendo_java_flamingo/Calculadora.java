1744d6328ac789d33d65a26f5b6249dcc2637133
Lucas_its4 <lucassouzadelima81@gmail.com>
2026-09-24 21:10:14 -0300

    Add files via upload

---
+++ aprendendo_java_flamingo/Calculadora.java
@@ -0,0 +1,16 @@
+package Aprendendo.Java.main.src.flamingo.intermediario.dominio;
+
+public class Calculadora {
+    public void soma(){
+        System.out.println(10 + 10);
+
+    }
+
+    public void subtrair() {
+        System.out.println(21 - 2);
+    }
+
+    public void  multiplicar(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+}
